a6ee389015c35fa5cb3e96fa20f1edce484a9a6d
Titus <flew2bits@gmail.com>
2022-05-26 16:05:20 -0400

    stage 1

---
+++ AddressBook/Entities/Person/Commands/CommandHandlers.cs
@@ -0,0 +1,21 @@
+﻿using AddressBook.Infrastructure.Data;
+using AddressBook.Infrastructure.Messages;
+
+namespace AddressBook.Entities.Person.Commands;
+
+public class CommandHandlers
+    : ICommandHandler<AddPersonCommand>
+{
+    private readonly PersonService _personService;
+
+    public CommandHandlers(PersonService personService)
+    {
+        _personService = personService;
+    }
+
+    public Task Handle(AddPersonCommand command, CancellationToken cancellationToken = default)
+    {
+        _personService.AddPerson(new Person(command.Id, command.FirstName, command.LastName));
+        return Task.CompletedTask;
+    }
+}
--- AddressBook/Entities/Person/PersonConfiguration.cs
+++ AddressBook/Entities/Person/PersonConfiguration.cs
@@ -1,9 +1,14 @@
-﻿namespace AddressBook.Entities.Person;
+﻿using AddressBook.Entities.Person.Commands;
+using AddressBook.Entities.Person.Queries;
+using AddressBook.Infrastructure.Messages;
+
+namespace AddressBook.Entities.Person;
 
 public static class PersonConfiguration
 {
     public static IServiceCollection AddPersonEntity(this IServiceCollection services)
-    {
-        return services;
-    }
+        => services
+            .AddQueryHandler<GetAllPersonQuery, IEnumerable<Person>, QueryHandlers>()
+            .AddQueryHandler<GetPersonByIdQuery, Person?, QueryHandlers>()
+            .AddCommandHandler<AddPersonCommand, CommandHandlers>();
 }
